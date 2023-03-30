b4d1d4cf1b499b7138c3cd103198c70e73688a42
nikhil-1e9 <84204563+nikhil-1e9@users.noreply.github.com>
2023-03-30 13:04:37 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,11 +4,33 @@ key = "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"
 
 import json
 import requests
+
 headers = {"Authorization": f"Bearer {key}"}
 API_URL = "https://api-inference.huggingface.co/models/gpt2"
+
 def query(payload):
     data = json.dumps(payload)
     response = requests.request("POST", API_URL, headers=headers, data=data)
     return json.loads(response.content.decode("utf-8"))
-data = query({"inputs": "Wanted to get some insughts as to what a Large Language Model can do"})
+
+# Stochastic methods
+top_p_params = {"max_length": 128, "top_p": 0.95, "do_sample":True}
+top_k_params = {"max_length": 128,  "top_k": 4, "do_sample":True}
+# Contrastive Search
+contrastive_params = {"max_length": 128, "penalty_alpha": 0.6, "top_k": 4}
+
+text = input()
+
+data = query({"inputs": text})
+# Improving performance using stochastic  and contrastive methods
+data1 = query({"inputs": text,
+               "top_p": top_p_params})
+data2 = query({"inputs": text,
+               "top_k": top_k_params})
+data3 = query({"inputs": text,
+               "parameters": contrastive_params})
+
 print(data)
+print("Top-p output:", data1)
+print("Top-k output:", data2)
+print("Contrastive search output:", data3)
